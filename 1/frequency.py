87ea2efb45d17d60868da729c0f75b72151e02c6
Cian Scolard <cian.scolard@datalex.com>
2018-12-03 16:44:54 +0000

    Day 1

---
+++ 1/frequency.py
@@ -0,0 +1,32 @@
+# AOC-2018 1.1 1.2
+import logging
+
+# log configutation
+log = logging.getLogger(__name__)
+log.setLevel(logging.INFO)
+ch = logging.StreamHandler()
+log.addHandler(ch)
+
+if __name__ == '__main__':
+    '''
+    '''
+    frequency = 0
+    found_frequencies = set()
+    duplicate_frequency = None
+
+    while(duplicate_frequency is None):
+        with open("input.txt") as content:
+            log.info("Loading input, frequency currently {0} and found_frequencies size {1}".format(frequency, len(found_frequencies)))
+            for line in content:
+                # Current frequency  0, change of +1; resulting frequency  1.
+                log.debug("Current frequency {0}, change of {1}; resulting frequency {2}".format(frequency, int(line), frequency + int(line)))
+                frequency = frequency + int(line)
+
+                if not (frequency in found_frequencies):
+                    found_frequencies.add(frequency)
+                else:
+                    duplicate_frequency = frequency
+                    log.info("Duplicate frequency {0} found.".format(frequency))
+                    break
+
+    log.info("Finished with final frequency {0}.".format(frequency))
